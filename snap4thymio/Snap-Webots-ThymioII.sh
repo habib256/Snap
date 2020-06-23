864776574ff877d9bb6c7ef39cd523c3ab53243c
habib256 <gist974@gmail.com>
2020-06-23 19:21:36 +0400

    Webots

---
+++ snap4thymio/Snap-Webots-ThymioII.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+####### Pour GNU/Linux
+cd ~/src/snap/snap4thymio/
+#./asebascratch -v -a ./thymio_motion.aesl
+#asebahttp  --aesl ./thymio_motion.aesl -s 33333 "ser:name=Thymio-II"
+#asebahttp2  --aesl ./thymio_motion.aesl -s 33333 "ser:name=Thymio-II"
+asebahttp -v "tcp:host=localhost;port=33333"
+
+
+########## Pour MACOSX
+#cd ~/snap/snap4thymio/thymio_motion.aesl
+#/Applications/Aseba/bin/asebahttp --aesl ./thymio_motion.aesl -s 33333 "ser:name=Thymio-II"
